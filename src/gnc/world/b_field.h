eedca07e23a25c4c123bf932c72b3f411be35189
Carson Lauer <lauercarson@gmail.com>
2025-06-05 23:00:16 -0700

    updated compute_B to take the r_ratio_n out of the loop at level n

--- src/gnc/world/b_field.h
+++ src/gnc/world/b_field.h
@@ -3,6 +3,9 @@
 #include "linalg.h"
 #include "slate.h"
 
+// compute_B computes the magnetic field
 void compute_B(slate_t *slate);
+// tests B field values
 void test_compute_B(slate_t *slate);
+// test_legendre_values
 void test_legendre_polynomials();
