45f8c1c6b78b9adb42f76aadb20487171d6fd1d6
Lundeen Cahilly <40362577+lundeen06@users.noreply.github.com>
2025-11-08 13:43:25 -0800

    64 safe state (#138)
    * Emergency & Safe States
    
    * Updated scheduler to send to safe state if below battery threshold
    
    * Remove unused states
    
    * Add safe mode voltage from AV
    
    * Update macros.h
    
    * Clean up states
    
    * Clean logs
    
    ---------
    
    Co-authored-by: Matthew Musson <matthewmmusson@gmail.com>

---
+++ src/states/safe_state.h
@@ -0,0 +1,15 @@
+/**
+ * @author Matthew Musson, Lundeen Cahilly
+ * @date 2025-11-08
+ *
+ * This file defines our default safe state that reads sensors and does nothing else
+ */
+
+#pragma once
+
+#include "scheduler/state_machine_types.h"
+#include "slate.h"
+
+sched_state_t *safe_get_next_state(slate_t *slate);
+
+extern sched_state_t safe_state;
